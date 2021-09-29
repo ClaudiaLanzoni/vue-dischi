c32a2efe0ece05c6ca999be57ef08e8d438340fa
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-29 16:20:22 +0200

    axios,bootstrap,fontsource install ; setting general.scss, variables.scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>prova di testo</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,6 +17,10 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import "./style/variables.scss";
+@import "./style/general.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
